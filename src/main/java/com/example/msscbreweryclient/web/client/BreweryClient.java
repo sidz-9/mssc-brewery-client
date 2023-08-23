40d7f296f48d54b10cd48ceb618bfcb148da1118
sidz-9 <sid.zaveri.dev@gmail.com>
2023-08-23 13:50:32 +0200

    implemented HTTP POST functionality with Spring RestTemplate. Created a test for the same.

---
+++ src/main/java/com/example/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,31 @@
+package com.example.msscbreweryclient.web.client;
+
+import com.example.msscbreweryclient.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+
+    public final String BEER_PATH_v1 = "/api/v1/beer/";
+    private String apiHost;
+    private final RestTemplate restTemplate;
+
+    // injecting RestTemplateBuilder reference into RestTemplate
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apiHost + BEER_PATH_v1 + uuid.toString(), BeerDto.class);
+    }
+
+    public void setApiHost(String apiHost) {
+        this.apiHost = apiHost;
+    }
+}
