a9ef11a215709a03e6f5507cacb77afcc654a125
sidz-9 <sid.zaveri.dev@gmail.com>
2023-08-23 13:50:32 +0200

    implemented HTTP GET functionality with Spring RestTemplate. Created a test for the same.

---
+++ src/test/java/com/example/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,26 @@
+package com.example.msscbreweryclient.web.client;
+
+import com.example.msscbreweryclient.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
+        assertNotNull(beerDto);
+        log.info(beerDto.toString());
+    }
+}
